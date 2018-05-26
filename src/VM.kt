972ab700a6cba41e3b7c91cc06f7882da375b0e9
Lucas Melin <lucas.melin@gmail.com>
2018-05-26 17:11:14 -0400

    Added loading rom into memory and added test rom.

--- src/VM.kt
+++ src/VM.kt
@@ -4,7 +4,7 @@ data class VM (
         val registers: ByteArray = ByteArray(16),
         var keys: ByteArray = ByteArray(16),
         var I: Int = 0, // 16-bit address register
-        var pc: Int = 0, // Program counter
+        var pc: Int = 0x200, // Program counter, starts at 0x200
         var sc: Int = 0, // Stack counter
         var delay: Int = 0, // Delay timer
         var sound: Int = 0 // Sound timer
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,19 @@
+import java.io.BufferedInputStream
+import java.io.DataInputStream
+import java.io.FileInputStream
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val vm = loadRom("roms/demos/Maze (alt) [David Winter, 199x].ch8")
+    println(vm.memory[0x200])
+}
+
+fun loadRom(file: String): VM {
+    // Open the rom in binary mode
+    return DataInputStream(BufferedInputStream(FileInputStream(file))).use {
+        val state = VM()
+        val rom = it.readBytes()
+        // Copy the entire rom array into state.memory starting the program counter
+        System.arraycopy(rom, 0, state.memory, state.pc, rom.size)
+        state
+    }
 }
