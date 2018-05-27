3d699b36dba48fe960a93254b61ed43db01fcd49
Lucas Melin <lucas.melin@gmail.com>
2018-05-26 22:48:52 -0400

    Adding more register opcodes based on the instruction manual
    http://devernay.free.fr/hacks/chip8/C8TECH10.HTM

--- src/Decoder.kt
+++ src/Decoder.kt
@@ -1,6 +1,11 @@
 interface Decoder {
-    fun call(address: Int) // 0NNN: Execute machine language subroutine at address NNN
-    fun clear() // 00E0: Clear the screen
+    fun sys(address: Int) // 0NNN: Execute machine language subroutine at address NNN
+    fun cls() // 00E0: Clear the screen
     fun ret() // 00EE: Return from a subroutine
-    fun jump() // 1NNN: Jump to address NNN
+    fun jp() // 1NNN: Jump to address NNN
+    fun call(address: Int) // 2NNN: Execute subroutine starting at address NNN
+    fun se(register: Int, value: Int) // 3XNN: Skip if Vx is equal to NN
+    fun sne(register: Int, value: Int) // 4XNN: Skip if Vx is not equal to NN
+    fun ser(registerx: Int, registery: Int) // 5XY0: Skip if Vx is equal to Vy
+    fun set(register: Int, value: Int) // 6XNN: Store the number NN in register Vx
 }
